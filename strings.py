5191e36f1f0f9d01288dbbf04a460b1ef7e71a87
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-17 22:16:54 +0000

    Add examples and explanations for single line and mulitiline string

---
+++ strings.py
@@ -0,0 +1,8 @@
+ #a string is a sequence of characters eclosed in a double or a single quote
+
+#single line string
+first_string = "Hello You!"
+
+#Multiline string can be achieved with a backslash
+multi_string = 'Hello are you '\
+'in the house?'
