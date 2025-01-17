ddbeda78adf7e21d817e702681c69dd14fe4cfa5
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-17 22:18:35 +0000

    Add examples of string reassignment and print statements

--- strings.py
+++ strings.py
@@ -6,3 +6,9 @@ first_string = "Hello You!"
 #Multiline string can be achieved with a backslash
 multi_string = 'Hello are you '\
 'in the house?'
+
+#strings can also be reassigned well, just like all variables except tuples i think haha!
+name = "mercy"
+print(name)
+name = "Angelo"
+print(name)
