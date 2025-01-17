861d8ecab6a48c51bced93d8de49545d046c7d40
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-17 21:58:25 +0000

    Add examples and print statements for string, list, and tuple data types

--- data_values.py
+++ data_values.py
@@ -9,7 +9,15 @@ complex_example = 10 + 10j
 print(type(complex_example))
 
 #SEQUENCE this contains one or more of the same type in an ordered list
+string_example = "john"
+print(type(string_example))
 
+list_example = [1,2,'fine', 4.5]
+print(type(list_example))
+
+#the values in a tuple cannot be changed
+tuple_example = (3,4, "tomb", 5.09)
+print(type(tuple_example))
 
 
 #dictionary 
