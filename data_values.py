7c63c54ade2b9b0eedf3e68bbae61d798e40dc27
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-17 21:57:23 +0000

    Add examples and print statements for integer, float, and complex data types

--- data_values.py
+++ data_values.py
@@ -1,5 +1,17 @@
 # numeric 
-#sequence
+integer_example = 10
+print(type(integer_example))
+
+float_example = 25.89
+print(type(float_example))
+
+complex_example = 10 + 10j
+print(type(complex_example))
+
+#SEQUENCE this contains one or more of the same type in an ordered list
+
+
+
 #dictionary 
 #boolean
 #set
