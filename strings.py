e4378d4a553e5f46ab49ba89b36c22f85cf3229f
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-17 22:20:38 +0000

    Rename variable for single line string for clarity

--- strings.py
+++ strings.py
@@ -1,7 +1,7 @@
  #a string is a sequence of characters eclosed in a double or a single quote
 
 #single line string
-first_string = "Hello You!"
+single_line_string = "Hello You!"
 
 #Multiline string can be achieved with a backslash
 multi_string = 'Hello are you '\
