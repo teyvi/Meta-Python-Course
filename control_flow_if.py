80ed85694459c12b2334c662ce8722b92b0e7b5e
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-18 23:49:50 +0000

    Update total_bill logic with additional discount conditions in control_flow_if.py

--- control_flow_if.py
+++ control_flow_if.py
@@ -1,11 +1,17 @@
 #control flow talks about the order inwhich the program runs e.g of flows conditional (if, else, if else) loop(for , while)
 
 #conditional statement
-total_bill = 45
+total_bill = 145
 discount1 = 10
+discount2 = 20
 
-if total_bill > 100:
+if total_bill > 100 and total_bill < 200:
     print("Bill is greater that 100!")
+    total_bill = total_bill - discount1
+elif total_bill > 200:
+    print('Bill is greater than 200')
+    total_bill = total_bill - discount2
+
 else:
     print("Bill is less than 100")
 
