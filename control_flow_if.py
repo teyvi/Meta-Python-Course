688ced85a89463331266958df34daaa521cb2305
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-18 23:45:55 +0000

    Add control flow examples with conditional statements in control_flow_if.py

---
+++ control_flow_if.py
@@ -0,0 +1,12 @@
+#control flow talks about the order inwhich the program runs e.g of flows conditional (if, else, if else) loop(for , while)
+
+#conditional statement
+total_bill = 45
+discount1 = 10
+
+if total_bill > 100:
+    print("Bill is greater that 100!")
+else:
+    print("Bill is less than 100")
+
+print("Total bill: " + str(total_bill))
