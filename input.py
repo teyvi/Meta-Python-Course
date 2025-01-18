ef84b9ff3b24e42178563d2540c4d867fdff205d
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-18 22:57:59 +0000

    used the format function to change position of outputs

--- input.py
+++ input.py
@@ -16,5 +16,9 @@ print(home_phone, cell_phone)
 date_of_birth = input('enter your year of birth: ')
 
 present_year = input('enter your present year: ')
-
+#using type casting to transform strings to ints
 print("Your age is" , int( present_year )- int(date_of_birth))
+
+#using format to print outputs
+age = int(date_of_birth) - int(present_year)
+print('Lets see.. born in {} and it is currently {}...you are {} old!' .format(date_of_birth,present_year,age))
