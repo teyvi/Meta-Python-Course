beb1da931aa1934297e237318ef6c6aeec4adbe3
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-18 23:40:07 +0000

    Add logical operators examples in operators.py

--- operators.py
+++ operators.py
@@ -11,3 +11,21 @@ print(3/4)
 
 #using the multiplication operator
 print(4*3)
+
+#logical operators AND OR and NOT
+#logical AND
+a = True
+b = True
+
+if a and b:
+    print("all true")
+
+#logical OR in a rigged paternity show
+
+fathers_dna = False
+mothers_dna = True
+
+if fathers_dna or mothers_dna:
+    print('Your are not the FATHER!')
+
+
