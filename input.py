9c6cdd081a03f0af78293779cd5a40380733328d
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-18 22:42:16 +0000

    Add input function to capture user input and print the result

--- input.py
+++ input.py
@@ -1,2 +1,7 @@
 #adding an input function to display 
-input('Please enter a number')
+#input('Please enter a number')
+
+num = input('Please enter a number: ')
+
+#using print to output input
+print(num)
