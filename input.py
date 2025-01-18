c1b8bd628e750e56ba99f89d31e7a33d8245f483
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-18 22:44:54 +0000

    Add functionality to capture multiple phone numbers from user input

--- input.py
+++ input.py
@@ -5,3 +5,9 @@ num = input('Please enter a number: ')
 
 #using print to output input
 print(num)
+
+#taking multiple inputs
+home_phone = input('Please enter your home phone number: ')
+
+cell_phone = input('Enter your cell phone number: ')
+print(home_phone, cell_phone)
