e897d36689034268e8ca909d9e3969d68051df53
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-28 08:20:35 +0000

    Add initial implementation of looping examples in loops.py

---
+++ loops.py
@@ -0,0 +1,9 @@
+#looping is a way to iterete over an sequence and access items in the sequence. There to constructs for loop and while loop
+
+#for loops
+#variable item is the placeholder for the current item in the sequence and favorites is the name of the sequence
+
+favorites = ['chocolate', 'ice cream', 'cake', 'cookies', 'candy']
+
+for item in favorites:
+    print('I love ', item)
